9bd950634ae15a1c45812f89e616c6c63ea6891b
Bastien PAUL <bastien.paul@qonto.com>
2022-07-28 09:28:50 +0200

    Add timber

--- app/src/main/java/com/example/template/App.kt
+++ app/src/main/java/com/example/template/App.kt
@@ -1,6 +1,15 @@
 package com.example.template
 
 import android.app.Application
+import timber.log.Timber
 
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupLog()
+    }
+
+    private fun setupLog() {
+        Timber.plant(Timber.DebugTree())
+    }
 }
